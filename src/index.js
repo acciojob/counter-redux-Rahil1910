2076bdf9daaa7ea3d09612ef37750eff45c585d4
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-14 22:38:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from "./components/store/store.jsx"
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
