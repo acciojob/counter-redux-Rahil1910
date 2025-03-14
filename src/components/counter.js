b17317101300f56b5bdb830810ae697f661742a3
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-14 22:40:50 +0530

    Create counter.js

---
+++ src/components/counter.js
@@ -0,0 +1,17 @@
+import {useSelector,useDispatch} from "react-redux";
+import {increment,decrement} from "./store/actions"
+
+const Counter = () =>{
+    const count =  useSelector((state)=> state.count);
+    const dispatch = useDispatch();
+
+    return(
+        <div>
+            <h1>Counter : {count}</h1>
+            <button onClick={()=> dispatch(increment())}>+</button>
+            <button onClick={()=> dispatch(decrement())}>-</button>
+        </div>
+    )
+}
+
+export default Counter;
